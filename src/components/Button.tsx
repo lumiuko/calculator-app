9ffd667dc59a2ce84ea4a5c21434256e266af670
David <kudavidss@gmail.com>
2024-02-08 04:46:26 +0400

    Button component improvements

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,23 @@
+type Color = 'primary' | 'secondary' | 'tertiary'
+
 type ButtonProps = {
-  color: 'primary' | 'secondary' | 'tertiary'
+  color: Color
   text: string
-  isWide?: boolean
-} & React.HTMLAttributes<HTMLButtonElement>
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
 
-export default function Button({ color = 'primary', text, isWide = false, ...props }: ButtonProps) {
-  const colorClasses = {
-    primary: 'bg-primary-background hover:bg-primary-background-hover text-primary-text shadow-primary text-xl md:text-2xl',
-    secondary: 'bg-secondary-background hover:bg-secondary-background-hover text-secondary-text shadow-secondary text-md md:text-lg',
-    tertiary: 'bg-tertiary-background hover:bg-tertiary-background-hover text-tertiary-text shadow-tertiary text-md md:text-lg'
-  }
+type ColorClasses = {
+  [key in Color]: string
+}
 
+const colorClasses: ColorClasses = {
+  primary: 'bg-primary-background hover:bg-primary-background-hover text-primary-text shadow-primary text-xl md:text-2xl',
+  secondary: 'bg-secondary-background hover:bg-secondary-background-hover text-secondary-text shadow-secondary text-md md:text-lg',
+  tertiary: 'bg-tertiary-background hover:bg-tertiary-background-hover text-tertiary-text shadow-tertiary text-md md:text-lg'
+}
+
+export default function Button({ color = 'primary', text, ...props }: ButtonProps) {
   return (
-    <button className={`rounded-[0.3125rem] py-3 leading-10 md:rounded-[0.625rem] ${colorClasses[color]} ${isWide ? '' : ''}`} {...props}>
+    <button className={`rounded-[0.3125rem] py-3 leading-10 md:rounded-[0.625rem] ${colorClasses[color]}`} {...props}>
       {text}
     </button>
   )
